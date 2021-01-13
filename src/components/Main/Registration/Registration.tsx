7480792ffef6fbdddfa763a59785ee6a12eafaee
lisapatrikeevna <lisa15.08patrikeevna@gmail.com>
2021-01-13 20:47:14 +0200

    add logout

--- src/components/Main/Registration/Registration.tsx
+++ src/components/Main/Registration/Registration.tsx
@@ -1,61 +1,86 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux';
-import {registrationTC} from "../../../redux/registration-reduser";
+import {deleteAccTC, registrationTC} from "../../../redux/registration-reduser";
 import Input from "../../common/Input/Input";
 import Button from "../../common/Button/Button";
 import {AppRootStateType} from "../../../redux/store";
+import cl from './Registration.module.css';
+import {Redirect} from 'react-router-dom';
 
 export const Registration = () => {
     const dispatch = useDispatch()
-    const message = useSelector<AppRootStateType,string>((state) => state.registration.error)
+    const errorMessage = useSelector<AppRootStateType, string>((state) => state.registration.error)
+    const message = useSelector<AppRootStateType, string>((state) => state.registration.message)
+    const registeredStatus = useSelector<AppRootStateType, boolean | null>((state) => state.registration.registeredStatus)
     let [password, setPassword] = useState<string>('')
     let [email, setEmail] = useState<string>('')
-    let [error, setError] = useState('')
+    let [error, setError] = useState<string>('')
 
-    // const onSendHandler=(text:string,type:string)=>{
-    //     if(text.trim()){
-    //         if(type==='email'){
-    //             setEmail(text)
-    //         }else {
-    //             setPassword(text)
-    //         }
-    //     }
-    // }
+
+    useEffect(() => {
+        if (registeredStatus) {
+            if (error === '') {
+                setEmail('')
+                setPassword('')
+            }
+        }
+    }, [registeredStatus])
+
     const onSendHandlerEmail = (text: string) => {
         // debugger
         if (text.trim()) {
+            setError('')
             setEmail(text)
-        } else {
-            setError('email is required')
         }
+        // else {
+        //     setError('email is required')
+        // }
     }
     const onSendHandlerPassw = (text: string) => {
         if (text.trim()) {
             setPassword(text)
-        } else {
-            setError('password is required')
         }
+        // else {
+        //     setError('password is required')
+        // }
 
     }
     const onClick = () => {
-        // debugger
+
         dispatch(registrationTC(email, password))
+
+
+    }
+    const deleteAcc = () => {
+        // debugger
+        dispatch(deleteAccTC())
+    }
+    //login: jhbjhj@mijni.ruj password: 12345678
+    if (registeredStatus) {
+        return <Redirect to={'/login'}/>
     }
     return (
-        <div className={'registration'}>
-            <h3>Registration</h3>
-            <>
-                <Input placeholder={'email'} type={'email'} onChangeText={onSendHandlerEmail}
-                       className={'default'} error={error} value={email}/>
-                {error && <p>error</p>}
-            </>
-            <>
-                <Input placeholder={'password'} type={'password'} onChangeText={onSendHandlerPassw}
-                       className={'default'} error={error} value={password}/>
-                {error && <p>error</p>}
-            </>
-            <Button onClick={onClick}>send</Button>
-            {message && <p>message</p>}
+        <div className={cl.registration}>
+            <div className={cl.center}>
+                <h3>Registration</h3>
+                <>
+                    <Input placeholder={'email'} type={'email'} onChangeText={onSendHandlerEmail}
+                           className={'default'} error={error} value={email}/>
+                </>
+                <>
+                    <Input placeholder={'password'} type={'password'} onChangeText={onSendHandlerPassw}
+                           className={'default'} error={error} value={password}/>
+                </>
+                <Button onClick={onClick}>send</Button>
+
+                {registeredStatus ? <p>{message}</p> : <p>{errorMessage}</p>}
+
+            </div>
+            <div className={cl.center}>
+                <h3>if you want to delete your account</h3>
+                <Button onClick={deleteAcc}>click me</Button>
+                <p>joke,it's logout</p>
+            </div>
         </div>
     )
 }
--- src/redux/registration-reduser.ts
+++ src/redux/registration-reduser.ts
@@ -6,14 +6,19 @@ export type  initialStateType = {
     email: string
     password: string
     error: string
+    message: string
+    registeredStatus: boolean | null
 }
 type registrationACType = ReturnType<typeof registrationAC>
 type answerRequestACType = ReturnType<typeof answerRequestAC>
-type actionType = registrationACType | answerRequestACType
+type succesRequestACType = ReturnType<typeof succesRequestAC>
+type actionType = registrationACType | answerRequestACType | succesRequestACType
 let initialState: initialStateType = {
     email: '',
     password: '',
     error: '',
+    message: '',
+    registeredStatus: null
 }
 const registerReduser = (state = initialState, action: actionType): initialStateType => {
     switch (action.type) {
@@ -21,35 +26,53 @@ const registerReduser = (state = initialState, action: actionType): initialState
             return {
                 ...state,
                 email: action.email,
-                password: action.password
+                password: action.password,
+            }
+        case "SUCCESHOST":
+            return {
+                ...state,
+                message: action.message,
+                registeredStatus: true
             }
         case "ERRORHOST":
             return {
                 ...state,
-                error: action.message
+                error: action.errorText,
+                registeredStatus: false
             }
         default:
             return state
     }
 }
 export const registrationAC = (email: string, password: string) => ({type: 'REGISTRATION', email, password} as const)
-export const answerRequestAC = (message: string) => ({type: 'ERRORHOST', message} as const)
+export const answerRequestAC = (errorText: string) => ({type: 'ERRORHOST', errorText} as const)
+export const succesRequestAC = (message:string) => ({type: 'SUCCESHOST',message} as const)
 export const registrationTC = (email: string, password: string) => (dispatch: Dispatch) => {
-        debugger
-        dispatch(registrationAC(email, password))
         RegistrationApi.setUser(email, password)
             .then(res => {
                 debugger
-                // dispatch(registrationAC(email, password))
                 console.log(res.data);
+                console.log(res.statusText);
+                let message = res.statusText
+                dispatch(registrationAC(email, password))
+                dispatch(succesRequestAC(message))
             }).catch((err) => {
-            let message = err.response ? err.response.data.errorText : err.message
-            console.log(err.response ? err.response.data.errorText : err.message);
-            dispatch(answerRequestAC(message))
+                debugger
+            // let errorText =  err.toString();
+            dispatch(answerRequestAC(err.response.data.error));
+            // console.log(errorText);
         })
-
-
     }
-
+export const deleteAccTC = () => (dispatch: Dispatch) =>{
+    debugger
+    RegistrationApi.deleteUser()
+        .then(res=>{
+            debugger
+            console.log(res);
+            // dispatch(succesRequestAC(message))
+        }).catch(err=>{
+            alert(err)
+    })
+}
 
 export default registerReduser;
