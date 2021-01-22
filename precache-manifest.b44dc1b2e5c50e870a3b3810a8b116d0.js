639ad324732737cbc02beba042be343b9736420d
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-01-22 13:38:00 +0300

    Updates

---
+++ precache-manifest.b44dc1b2e5c50e870a3b3810a8b116d0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ad45e0a464f7583d537352b63d92451",
+    "url": "/cards/index.html"
+  },
+  {
+    "revision": "07ec1bde30bcd28610c6",
+    "url": "/cards/static/css/main.b09cb1ac.chunk.css"
+  },
+  {
+    "revision": "65a41fb5b1c6a2372600",
+    "url": "/cards/static/js/2.2da7f4be.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "/cards/static/js/2.2da7f4be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c2f4f52f023fdf1193",
+    "url": "/cards/static/js/3.e446f748.chunk.js"
+  },
+  {
+    "revision": "07ec1bde30bcd28610c6",
+    "url": "/cards/static/js/main.acf858e1.chunk.js"
+  },
+  {
+    "revision": "6b9e36716d3ed5c1a8d2",
+    "url": "/cards/static/js/runtime-main.2683ab14.js"
+  }
+]);
